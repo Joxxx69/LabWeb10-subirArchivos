2852d1573ce3390f67437003334d3ef5f7312c98
Joxxx69 <santiago.gualotuna@hotmail.com>
2023-08-16 18:39:44 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fileUpload = require('express-fileupload')
+
+const app = express();
+const port = 8000;
+
+app.use(express.static())
+app.use(fileUpload());
+
+
+
+app.post('/upload',(req,res) => {
+    let EDFile = req.files.file
+    EDFile.mv(`./files/${EDFile.name}`,err => {
+        if(err) return res.status(500).send({ message : err })
+
+        return res.status(200).send({ message : 'File upload' })
+    })
+})
+
+
+app.listen(port, () => {
+    console.log('conexion establecida');
+})
